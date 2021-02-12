b05d81229c42b84417ff54287cccf65ce7b6bf37
unknown <varvarchik3@gmail.com>
2021-02-12 18:09:26 +0200

    protected inheritance example

--- main.cpp
+++ main.cpp
@@ -6,42 +6,32 @@ class base
 {
 private:
 	int a;
+protected:
 	int b;
 public:
-	base() { cout << "Base constructor" << endl; a = 10; b = 20; }
-	void showBase() { cout << "a = " << a << ", b = " << b << endl; }
-	~base() { cout << "Base destructor" << endl; }
+	int c;
+	void show() { cout << "a = " << a << ", b = " << b << ", c = " << c << endl; }
+	void set() { a = 100; b = 100; c = 300; }
 };
 
-class derive : public base
+class derive : protected base
 {
 private:
-	int c;
+	
 public:
-	derive() { cout << "Derive constructor" << endl; c = 30; }
-	void showDerive() { cout << "c = " << c << endl; }
-	~derive() { cout << "Derive destructor" << endl; }
+	void setB(int val) { b = val; }
+	void setC(int val) { c = val; }
+	void show() { cout << "b = " << b << ", c = " << c << endl; }
 };
 
-class derive2 : public derive
-{
-	private:
-		int d;
-public:
-	derive2() { cout << "Derive2 constructor" << endl; d = 100; }
-	void showDerive2() { cout << "d = " << d << endl; }
-	~derive2() { cout << "Derive2 destructor" << endl; }
-};
+
 
 int main()
 {
 	derive d;
-	d.showBase();
-	d.showDerive();
-	derive2 d2;
-	d2.showBase();
-	d2.showDerive();
-	d2.showDerive2();
+	d.setB(2);
+	d.setC(3);
+	d.show();
 
 	return 0;
 }
